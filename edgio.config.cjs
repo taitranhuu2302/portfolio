44ef039a3659681d95699600da70f75d79243557
Tai Tran <tai.tran@mgisolutions.com>
2024-01-10 17:59:04 +0700

    update

--- edgio.config.cjs
+++ edgio.config.cjs
@@ -1,3 +1,5 @@
+const {join} = require('path');
+
 // This file was automatically added by edgio init.
 // You should commit this file to source control.
 // Learn more about this file at https://docs.edg.io/guides/edgio_config
@@ -5,18 +7,18 @@ module.exports = {
   connector: '@edgio/astro',
 
   // The name of the site in Edgio to which this app should be deployed.
-  // name: 'my-site-name',
+  name: 'tai-tran',
 
   // The name of the organization in Edgio to which this app should be deployed.
   // organization: 'my-organization-name',
 
   // Overrides the default path to the routes file. The path should be relative to the root of your app.
-  // routes: 'routes.js',
+  routes: 'routes.ts',
 
     astro: {
     // The path of the standalone server that runs Astro SSR
     // The dependencies for this file are automatically bundled
-    // appPath: join(process.cwd(), 'dist', 'server', 'entry.mjs')
+    appPath: join(process.cwd(), 'dist', 'server', 'entry.mjs')
   },
 
   // When set to true or omitted entirely, Edgio includes the deployment number in the cache key,
--- routes.ts
+++ routes.ts
@@ -1,7 +1,7 @@
 // This file was automatically added by edgio init.
 // You should commit this file to source control.
 import { Router } from '@edgio/core'
-import { connectorRoutes } from '@edgio/connectors'
+import { astroRoutes } from '@edgio/astro'
 
-export default new Router().use(connectorRoutes)
+export default new Router().use(astroRoutes)
 
