2ee2a55079816e8959e8df69723330a1d2e007ee
Tai Tran <tai.tran@mgisolutions.com>
2024-01-12 10:56:45 +0700

    update

--- routes.ts
+++ routes.ts
@@ -2,7 +2,7 @@
 // You should commit this file to source control.
 import { Router } from "@edgio/core";
 import { astroRoutes } from "@edgio/astro";
-import { ASSET_CACHE_HANDLER, PAGE_CACHE_HANDLER } from "./cache";
+import { ASSET_CACHE_HANDLER, IMAGE_CACHE_HANDLER } from "./cache";
 
 export default new Router()
   .match("/", ASSET_CACHE_HANDLER)
@@ -10,7 +10,7 @@ export default new Router()
   .match("/about", ASSET_CACHE_HANDLER)
   .match("/404", ASSET_CACHE_HANDLER)
   .match("/_astro/:path*", ASSET_CACHE_HANDLER)
-  .match("/assets/:path*", ASSET_CACHE_HANDLER)
+  .match("/assets/:path*", IMAGE_CACHE_HANDLER)
   .match("/assets/backgrounds/:path*", ASSET_CACHE_HANDLER)
   .match("/work/:path*", ASSET_CACHE_HANDLER)
   .use(astroRoutes);
