4e766ac7414e2590a7859724a7b5b7b322ee3f0e
Tai Tran <tai.tran@mgisolutions.com>
2024-01-12 10:44:20 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ export default defineConfig({
   output: "server",
   adapter: node({
     mode: "standalone"
-  })
+  }),
 });
--- routes.ts
+++ routes.ts
@@ -1,7 +1,16 @@
 // This file was automatically added by edgio init.
 // You should commit this file to source control.
-import { Router } from '@edgio/core'
-import { astroRoutes } from '@edgio/astro'
+import { Router } from "@edgio/core";
+import { astroRoutes } from "@edgio/astro";
+import { ASSET_CACHE_HANDLER, PAGE_CACHE_HANDLER } from "./cache";
 
-export default new Router().use(astroRoutes)
-
+export default new Router()
+  .match("/", ASSET_CACHE_HANDLER)
+  .match("/work", ASSET_CACHE_HANDLER)
+  .match("/about", ASSET_CACHE_HANDLER)
+  .match("/404", ASSET_CACHE_HANDLER)
+  .match("/_astro/:path*", ASSET_CACHE_HANDLER)
+  .match("/assets/:path*", ASSET_CACHE_HANDLER)
+  .match("/assets/backgrounds/:path*", ASSET_CACHE_HANDLER)
+  .match("/work/:path*", ASSET_CACHE_HANDLER)
+  .use(astroRoutes);
